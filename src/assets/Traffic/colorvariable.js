1c2da0f2306ed828ccd5c607b2b04926de0dd49b
kyber03 <102473674+kyber03@users.noreply.github.com>
2024-12-01 20:57:38 +0530

    Create colorvariable.js
    Color variables , export accordingly

---
+++ src/assets/Traffic/colorvariable.js
@@ -0,0 +1,18 @@
+export const trafficState = {
+  red: {
+    duration: 4000,
+    backgroundColor: "red",
+    next: "green",
+  },
+  yellow: {
+    duration: 500,
+    backgroundColor: "yellow",
+    next: "red",
+  },
+  green: {
+    duration: 3000,
+    backgroundColor: "green",
+    next: "yellow",
+  },
+};
+
